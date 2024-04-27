253a193ca9d32d8d9f28f8775eba332f3bdcda90
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-04-28 02:47:54 +0500

    Commit

---
+++ 1-lesson/src/lesson3.java
@@ -0,0 +1,13 @@
+public class lesson3 {
+    public static void main(String[] args) {
+        String name = "Abdulmajid";
+        System.out.println("Hello " + name);
+
+        String firstName = "Abdulmajid";
+        String lastName = "Mirmakhmudov";
+        String fullName = lastName + " " + firstName;
+        System.out.println(fullName);
+
+
+    }
+}
---
+++ 1-lesson/src/lesson4.java
@@ -0,0 +1,12 @@
+public class lesson4 {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 20;
+
+        System.out.println(number1 + number2);
+
+        System.out.println(number1 * number2);
+
+        System.out.println(number1 - number2);
+    }
+}
