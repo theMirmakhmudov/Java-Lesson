2679994bf915704be3a37970d90d495f0e309e2d
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-04-28 02:56:52 +0500

    Commit

--- 1-lesson/src/lesson4.java
+++ 1-lesson/src/lesson4.java
@@ -1,6 +1,6 @@
 public class lesson4 {
     public static void main(String[] args) {
-        int number1 = 10;
+        int number1 = 30;
         int number2 = 20;
 
         System.out.println(number1 + number2);
@@ -8,5 +8,11 @@ public class lesson4 {
         System.out.println(number1 * number2);
 
         System.out.println(number1 - number2);
+
+        System.out.println(number1 / number2);
+
+        System.out.println(number1 % number2);
+
+        System.out.println(number1 % number2);
     }
 }
---
+++ 1-lesson/src/lesson5.java
@@ -0,0 +1,10 @@
+public class lesson5 {
+    public static void main(String[] args) {
+        String txt = "Abdulmajid";
+        System.out.println("The length of the txt string is: " + txt.length());
+
+        String text = "Front-end";
+        System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+        System.out.println(txt.toLowerCase());  // Outputs "hello world"
+    }
+}
