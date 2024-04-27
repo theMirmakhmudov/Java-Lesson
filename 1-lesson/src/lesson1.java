07ca55a504256ffec94dd1117629e87b8b958d8c
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-04-28 02:41:55 +0500

    Commit

--- 1-lesson/src/lesson1.java
+++ 1-lesson/src/lesson1.java
@@ -1,5 +1,6 @@
-public class Main {
+public class lesson1 {
     public static void main(String[] args) {
         System.out.print("Hello and welcome!");
     }
 }
+
---
+++ 1-lesson/src/lesson2.java
@@ -0,0 +1,21 @@
+public class lesson2 {
+    public static void main(String[] args) {
+        String name = "Abdulmajid";
+        System.out.println(name);
+
+        int number = 16;
+        System.out.println(number);
+
+        float amount = 100.02f;
+        System.out.println(amount);
+
+        char letter = 'M';
+        System.out.println(letter);
+
+        boolean check1 = true;
+        boolean check2 = false;
+        System.out.println(check1);
+        System.out.println(check2);
+
+    }
+}
